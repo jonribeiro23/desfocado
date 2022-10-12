775bab55ec0cb38102d0c2512f14789f0797ceb5
Jonathas Ribeiro <jonathas_tc@hotmail.com>
2022-10-12 13:31:36 -0300

    Realizando upload de imagens

--- flickr_api/__init__.py
+++ flickr_api/__init__.py
@@ -1,17 +1,51 @@
 import flickrapi
-from credentials import FLICKR_KEY, FLICKR_SECRET
+from credentials import FLICKR_KEY, FLICKR_SECRET, USER_ID
 import json
+import webbrowser
+
 
 class Flickr:
     def __init__(self) -> None:
-        self.flickr = flickrapi.FlickrAPI(FLICKR_KEY, FLICKR_SECRET, format='json')
+        self.flickr = flickrapi.FlickrAPI(FLICKR_KEY, FLICKR_SECRET)
+        self.user_id = USER_ID
+
+        self._verify_permissions()
+
+    def photo_info(self):
+        photos = self.flickr.photos.search(user_id=self.user_id)
+        res = json.loads(photos.decode('utf-8'))
+        return res['photos']['photo']
+
+    def _mount_photo_url(self, photo):
+        return f"https://live.staticflickr.com/{photo['server']}/{photo['id']}_{photo['secret']}_b.jpg"
+
+    def get_photos_url(self):
+        photos = self.photo_info()
+        for photo in photos:
+            yield {
+                'url': self._mount_photo_url(photo)
+            }
+
+    def upload(self, photo):
+        return self.flickr.upload(photo)
+
+    def _verify_permissions(self):
+        if not self.flickr.token_valid(perms='write'):
+
+            # Get a request token
+            self.flickr.get_request_token(oauth_callback='oob')
+
+            # Open a browser at the authentication URL. Do this however
+            # you want, as long as the user visits that URL.
+            authorize_url = self.flickr.auth_url(perms='write')
+            webbrowser.open_new_tab(authorize_url)
+
+            # Get the verifier code from the user. Do this however you
+            # want, as long as the user gives the application the code.
+            verifier = str(input('Verifier code: '))
+
+            # Trade the request token for an access token
+            self.flickr.get_access_token(verifier)
 
     def test(self):
-        # photos = self.flickr.photos.search(user_id='73509078@N00', per_page='10')
-        # [print(i.getInfo()) for i in photos]
-        # return photos
-
-        raw = self.flickr.photosets.getList(user_id='73509078@N00')
-        # print( json.loads(raw.decode('utf-8')))
-        return json.loads(raw.decode('utf-8'))
-        
+        return self.get_photo_url('52422862629')
--- test.py
+++ test.py
@@ -1,8 +1,59 @@
+from itertools import count
+from numpy import array
 from flickr_api import Flickr
 from pprint import pprint
+import os.path
+from os import walk
 
 t = Flickr()
-pho = t.test()
-pprint(pho['photosets']['photoset'])
-# for i in pho['photosets']['total']:
+# pho = t.get_photos_url()
+# pprint(pho)
+# for i in pho:
 #     print(i)
+
+class FilePhoto(object):
+    def __init__(self, filename, callback):
+        self.file = open(filename, 'rb')
+        self.callback = callback
+        # the following attributes and methods are required
+        self.len = os.path.getsize(filename)
+        self.fileno = self.file.fileno
+        self.tell = self.file.tell
+
+    def read(self, size):
+        if self.callback:
+            self.callback(self.tell() * 100 // self.len)
+        return self.file.read(size)
+
+
+def callback(progress):
+    print(progress)
+
+
+class Photo(object):
+    def __init__(self, filename, fileobj) -> None:
+        self.filename = filename
+        self.fileobj = fileobj
+
+params = {'filename': 'img/IMG_3520.JPG'}
+params['fileobj'] = FilePhoto(params['filename'], callback)
+
+# photo = t.upload(params)
+
+files = []
+for (dirpath, dirnames, filenames) in walk('img'):
+    qtd = len(filenames)
+    counter = 1
+    for photo in filenames:
+        try:
+            t.upload(str(f"{dirpath}/{photo}"))
+            print(100 * (counter / qtd) + photo)
+        except Exception:
+            pass
+        counter += 1
+
+# res = t.upload('img/DSCF8850.jpg')
+
+# print(res.attrib)
+
+# t.upload(photo)
