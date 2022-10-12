14b6bcddc8cbca56868f748d8615d933bce926aa
Jonathas Ribeiro <jonathas_tc@hotmail.com>
2022-10-12 15:32:32 -0300

    deletando fotos

--- flickr_api/__init__.py
+++ flickr_api/__init__.py
@@ -3,28 +3,12 @@ from credentials import FLICKR_KEY, FLICKR_SECRET, USER_ID
 import json
 import webbrowser
 
-
-class Flickr:
+class FlickerUpload:
     def __init__(self) -> None:
         self.flickr = flickrapi.FlickrAPI(FLICKR_KEY, FLICKR_SECRET)
         self.user_id = USER_ID
 
         self._verify_permissions()
-
-    def photo_info(self):
-        photos = self.flickr.photos.search(user_id=self.user_id)
-        res = json.loads(photos.decode('utf-8'))
-        return res['photos']['photo']
-
-    def _mount_photo_url(self, photo):
-        return f"https://live.staticflickr.com/{photo['server']}/{photo['id']}_{photo['secret']}_b.jpg"
-
-    def get_photos_url(self):
-        photos = self.photo_info()
-        for photo in photos:
-            yield {
-                'url': self._mount_photo_url(photo)
-            }
 
     def upload(self, photo):
         return self.flickr.upload(photo)
@@ -32,20 +16,56 @@ class Flickr:
     def _verify_permissions(self):
         if not self.flickr.token_valid(perms='write'):
 
-            # Get a request token
             self.flickr.get_request_token(oauth_callback='oob')
 
-            # Open a browser at the authentication URL. Do this however
-            # you want, as long as the user visits that URL.
             authorize_url = self.flickr.auth_url(perms='write')
             webbrowser.open_new_tab(authorize_url)
 
-            # Get the verifier code from the user. Do this however you
-            # want, as long as the user gives the application the code.
             verifier = str(input('Verifier code: '))
-
-            # Trade the request token for an access token
             self.flickr.get_access_token(verifier)
 
-    def test(self):
-        return self.get_photo_url('52422862629')
+
+class Flickr:
+    def __init__(self) -> None:
+        self.flickr = flickrapi.FlickrAPI(FLICKR_KEY, FLICKR_SECRET, format='json')
+        self.user_id = USER_ID
+
+        self._verify_permissions()
+
+    def photo_info(self):
+        photos = self.flickr.photos.search(user_id=self.user_id, extras='url_c')
+        res = json.loads(photos.decode('utf-8'))
+        return res['photos']['photo']
+
+    def get_photos_url(self):
+        photos = self.photo_info()
+        for photo in photos:
+            yield {
+                'url': photo['url_c']
+            }
+
+    def delete(self, photo_id):
+        return self.flickr.photos.delete(photo_id=photo_id)
+
+    def _verify_permissions(self):
+        if not self.flickr.token_valid(perms='write'):
+
+            self.flickr.get_request_token(oauth_callback='oob')
+
+            authorize_url = self.flickr.auth_url(perms='write')
+            webbrowser.open_new_tab(authorize_url)
+
+            verifier = str(input('Verifier code: '))
+            self.flickr.get_access_token(verifier)
+
+        if not self.flickr.token_valid(perms='delete'):
+
+            self.flickr.get_request_token(oauth_callback='oob')
+
+            authorize_url = self.flickr.auth_url(perms='delete')
+            webbrowser.open_new_tab(authorize_url)
+
+            verifier = str(input('Verifier code: '))
+
+            self.flickr.get_access_token(verifier)
+
